a0c1d2e32cc5e01731d386f0bf4aab3c9affe71a
Agustín Azzinnari <aazzinnari@gmail.com>
2019-09-10 11:08:58 -0300

    Add second stage of RetinaFace, the pyramid refiner

--- terran/face/detection/__init__.py
+++ terran/face/detection/__init__.py
@@ -2,6 +2,7 @@ import numpy as np
 import mxnet as mx
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 from itertools import chain
 from PIL import Image
@@ -110,10 +111,13 @@ def _load_from_mxnet(path):
     return state_dict
 
 
-class ConvBlock(nn.Module):
+class ConvSepBlock(nn.Module):
 
     def __init__(self, in_c, out_c, stride=1, return_both=False):
-        """Building block for base network, consisting of Conv, BN and ReLU.
+        """Building block for base network.
+
+        Consists of common Conv, BN and ReLU sequence, followed by the same
+        sequence but with a separable Conv.
 
         Arguments:
             return_both (bool): Return the outputs of both inner components,
@@ -174,27 +178,27 @@ class BaseNetwork(nn.Module):
         # retrieve them easily enough on the `forward` pass.
         self.scales = nn.ModuleList([
             nn.Sequential(
-                ConvBlock(8, 16, stride=2),
+                ConvSepBlock(8, 16, stride=2),
 
-                ConvBlock(16, 32),
-                ConvBlock(32, 32, stride=2),
+                ConvSepBlock(16, 32),
+                ConvSepBlock(32, 32, stride=2),
 
-                ConvBlock(32, 64),
-                ConvBlock(64, 64, stride=2, return_both=True),
+                ConvSepBlock(32, 64),
+                ConvSepBlock(64, 64, stride=2, return_both=True),
             ),
 
             nn.Sequential(
-                ConvBlock(64, 128),
-                ConvBlock(128, 128),
-                ConvBlock(128, 128),
-                ConvBlock(128, 128),
-                ConvBlock(128, 128),
-                ConvBlock(128, 128, stride=2, return_both=True),
+                ConvSepBlock(64, 128),
+                ConvSepBlock(128, 128),
+                ConvSepBlock(128, 128),
+                ConvSepBlock(128, 128),
+                ConvSepBlock(128, 128),
+                ConvSepBlock(128, 128, stride=2, return_both=True),
             ),
         ])
 
         self.final_conv = nn.Sequential(
-            ConvBlock(128, 256),
+            ConvSepBlock(128, 256),
             nn.Conv2d(256, 256, 1, bias=False),
             nn.BatchNorm2d(256, momentum=0.9),
             nn.ReLU(),
@@ -212,3 +216,121 @@ class BaseNetwork(nn.Module):
         feature_maps.append(out)
 
         return feature_maps
+
+
+class ContextModule(nn.Module):
+    """Context module to expand the receptive field of the feature map.
+
+    Every point in the feature map will be a mixture of a 3x3, a 5x5 and a 7x7
+    receptive fields. The first 128 channels correspond to the 3x3 one, and the
+    remaining 64 and 64 to the rest.
+    """
+
+    def __init__(self):
+        super().__init__()
+
+        self.context_3x3 = nn.Sequential(
+            nn.Conv2d(256, 128, 3, padding=1),
+            nn.BatchNorm2d(128, momentum=0.9),
+            nn.ReLU(),
+        )
+
+        self.dimension_reducer = nn.Sequential(
+            nn.Conv2d(256, 64, 3, padding=1),
+            nn.BatchNorm2d(64, momentum=0.9),
+            nn.ReLU(),
+        )
+
+        self.context_5x5 = nn.Sequential(
+            nn.Conv2d(64, 64, 3, padding=1),
+            nn.BatchNorm2d(64, momentum=0.9),
+            nn.ReLU(),
+        )
+
+        self.context_7x7 = nn.Sequential(
+            nn.Conv2d(64, 64, 3, padding=1),
+            nn.BatchNorm2d(64, momentum=0.9),
+            nn.ReLU(),
+
+            nn.Conv2d(64, 64, 3, padding=1),
+            nn.BatchNorm2d(64, momentum=0.9),
+            nn.ReLU(),
+        )
+
+    def forward(self, x):
+        red = self.dimension_reducer(x)
+
+        # By applying successive 3x3 convolutions, we get an effective
+        # receptive field of 5x5 and 7x7.
+        ctx_3x3 = self.context_3x3(x)
+        ctx_5x5 = self.context_5x5(red)
+        ctx_7x7 = self.context_7x7(red)
+
+        out = torch.cat([ctx_3x3, ctx_5x5, ctx_7x7], dim=1)
+
+        return out
+
+
+class PyramidRefiner(nn.Module):
+    """Refines the feature pyramids from the base network into usable form.
+
+    Normalizes channel sizes, mixes them up, and runs them through the context
+    module.
+    """
+
+    def __init__(self):
+        super().__init__()
+
+        self.conv_stride8 = nn.Sequential(
+            nn.Conv2d(64, 256, 1),
+            nn.BatchNorm2d(256, momentum=0.9),
+            nn.ReLU(),
+        )
+        self.conv_stride16 = nn.Sequential(
+            nn.Conv2d(128, 256, 1),
+            nn.BatchNorm2d(256, momentum=0.9),
+            nn.ReLU(),
+        )
+        self.conv_stride32 = nn.Sequential(
+            nn.Conv2d(256, 256, 1),
+            nn.BatchNorm2d(256, momentum=0.9),
+            nn.ReLU(),
+        )
+
+        self.context_stride8 = ContextModule()
+        self.context_stride16 = ContextModule()
+        self.context_stride32 = ContextModule()
+
+    def forward(self, x):
+        """Forward pass for refiner.
+
+        Expects `x` to be an array of three tensors, one per feature. Returns
+        the same.
+        """
+        stride8, stride16, stride32 = x
+
+        # First run every scale through the conv layers.
+        proc_stride8 = self.conv_stride8(stride8)
+        proc_stride16 = self.conv_stride16(stride16)
+        proc_stride32 = self.conv_stride32(stride32)
+
+        # Upsample the smaller to the bigger ones and sum. When upsampling,
+        # resulting tensor might be bigger by one spatial dimension (if initial
+        # dimension wasn't power of 2), so we slice the tensor to the same
+        # size.
+        ups_stride32 = F.interpolate(
+            proc_stride32, scale_factor=2
+        )[:, :, :proc_stride16.shape[2], :proc_stride16.shape[3]]
+        proc_stride16 = proc_stride16 + ups_stride32
+
+        ups_stride16 = F.interpolate(
+            proc_stride16, scale_factor=2
+        )[:, :, :proc_stride8.shape[2], :proc_stride8.shape[3]]
+        proc_stride8 = proc_stride8 + ups_stride16
+
+        # Now run every scale through a context module.
+        ctx_stride8 = self.context_stride8(proc_stride8)
+        ctx_stride16 = self.context_stride16(proc_stride16)
+        ctx_stride32 = self.context_stride32(proc_stride32)
+
+        return [ctx_stride8, ctx_stride16, ctx_stride32]
