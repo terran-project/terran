a4b18fdbc07fb175bd16006a3579f0accc5b96f8
Agustín Azzinnari <aazzinnari@gmail.com>
2019-09-21 11:12:41 -0300

    Remove unused preprocessing code and simplify

--- terran/face/recognition/arcface_wrapper.py
+++ terran/face/recognition/arcface_wrapper.py
@@ -20,95 +20,81 @@ def load_model():
     return model
 
 
-def preprocess_face(
-    img, bbox=None, landmark=None, image_size=(112, 112), margin=44
-):
-    """Preprocess an image by aligning the face contained in them."""
-    M = None
+def preprocess_face(image, landmark, image_size=(112, 112)):
+    """Prepares the face image for the recognition model.
 
-    if landmark is not None:
-        # Target location of the facial landmarks.
-        src = np.array(
-          [
-            [30.2946, 51.6963],
-            [65.5318, 51.5014],
-            [48.0252, 71.7366],
-            [33.5493, 92.3655],
-            [62.7299, 92.2041]
-          ],
-          dtype=np.float32
-        )
+    Uses the detected landmarks from the face detection stage, then aligns
+    the image, pads to `image_side`x`image_side`, and turns it into the BGR
+    CxHxW format.
 
-        if image_size[1] == 112:
-            src[:, 0] += 8.0
+    Parameters
+    ----------
+    image : np.ndarray of size HxWxC.
+        Image containing a face to preprocess.
+    landmark : np.ndarray of size (5, 2).
+        Landmark coordinates.
 
-        dst = landmark.astype(np.float32)
+    """
 
-        tform = SimilarityTransform()
-        tform.estimate(dst, src)
-        M = tform.params[0:2, :]
+    # Target location of the facial landmarks.
+    src = np.array([
+        [30.2946, 51.6963],
+        [65.5318, 51.5014],
+        [48.0252, 71.7366],
+        [33.5493, 92.3655],
+        [62.7299, 92.2041]
+    ], dtype=np.float32)
 
-    if M is None:
-        if bbox is None:  # Use center crop.
-            det = np.zeros(4, dtype=np.int32)
-            det[0] = int(img.shape[1]*0.0625)
-            det[1] = int(img.shape[0]*0.0625)
-            det[2] = img.shape[1] - det[0]
-            det[3] = img.shape[0] - det[1]
-        else:
-            det = bbox
+    if image_size[1] == 112:
+        src[:, 0] += 8.0
 
-        bb = np.zeros(4, dtype=np.int32)
-        bb[0] = np.maximum(det[0]-margin/2, 0)
-        bb[1] = np.maximum(det[1]-margin/2, 0)
-        bb[2] = np.minimum(det[2]+margin/2, img.shape[1])
-        bb[3] = np.minimum(det[3]+margin/2, img.shape[0])
+    dst = landmark.astype(np.float32)
 
-        ret = img[bb[1]:bb[3], bb[0]:bb[2], :]
-        ret = cv2.resize(ret, (image_size[1], image_size[0]))
+    tform = SimilarityTransform()
+    tform.estimate(dst, src)
+    M = tform.params[0:2, :]
 
-        return ret
-    else:
-        # Do align using landmark.
-        warped = cv2.warpAffine(
-          img, M, (image_size[1], image_size[0]), borderValue=0.0
-        )
-        return warped
+    # Do align using landmark.
+    warped = cv2.warpAffine(
+        image, M, (image_size[1], image_size[0]), borderValue=0.0
+    )
+
+    return warped.transpose([2, 0, 1])[::-1, ...]
+
+
+def preprocess_face_no_landmarks(image, image_side=112):
+    """Preprocess a face without landmarks.
+
+    Resize image to have side `image_side` and add padding around it.
+    """
+    face = Image.fromarray(image)
+
+    scale = image_side / max(face.size[0], face.size[1])
+    face = face.resize(
+        (int(face.size[0] * scale), int(face.size[1] * scale))
+    )
+
+    x_min = int((image_side - face.size[0]) / 2)
+    x_max = int((image_side - face.size[0]) / 2) + face.size[0]
+    y_min = int((image_side - face.size[1]) / 2)
+    y_max = int((image_side - face.size[1]) / 2) + face.size[1]
+
+    preprocessed = np.zeros(
+        (3, image_side, image_side), dtype=np.uint8
+    )
+    preprocessed[:, y_min:y_max, x_min:x_max] = (
+        np.asarray(face).transpose([2, 0, 1])[::-1, ...]
+    )
+
+    return preprocessed
 
 
 class ArcFace:
 
-    def __init__(self, device=default_device, threshold=1.24, image_side=112):
+    def __init__(self, device=default_device, image_side=112):
         self.device = device
         self.model = load_model().to(self.device)
-
-        self.det_threshold = [0.6, 0.7, 0.8]
         self.image_side = image_side
-        self.threshold = threshold
-
-    def get_input(self, image, face):
-        """Prepares the face image for the recognition model.
-
-        Uses the detected landmarks from the face detection stage, then aligns
-        the image, pads to `image_side`x`image_side`, and turns it into the BGR
-        CxHxW format.
-
-        TODO: If no face.
-
-        Parameters
-        ----------
-        image : np.ndarray of size HxWxC.
-
-        """
-        bbox = face['bbox']
-        points = face['landmarks']
-
-        # TODO: Move all this to `preprocess_face` and remove this function.
-        processed = preprocess_face(image, bbox, points)
-        processed = cv2.cvtColor(processed, cv2.COLOR_BGR2RGB)
-        aligned = np.transpose(processed, (2, 0, 1))
-
-        return aligned
 
     def call(self, images, faces_per_image=None):
         """Performs feature extraction on `images`.
@@ -142,39 +128,19 @@ class ArcFace:
         if faces_per_image is not None:
             for image, faces in zip(images, faces_per_image):
                 for face in faces:
-                    preprocessed.append(self.get_input(image, face))
+                    preprocessed.append(
+                        preprocess_face(image, face['landmarks'])
+                    )
 
             # A bit of magic, but gets us the index within `preprocessed` where
             # faces of each image start.
             splits = np.cumsum(list(map(len, faces_per_image)))[:-1]
         else:
-            # No landmarks provided, so preprocess it manually: resize
-            # image to `image_size` and add padding around it.
+            # No landmarks provided, so preprocess it manually.
             for image in images:
-                face = Image.fromarray(image)
-
-                scale = self.image_side / max(face.size[0], face.size[1])
-                face = face.resize(
-                    (int(face.size[0] * scale), int(face.size[1] * scale))
+                preprocessed.append(
+                    preprocess_face_no_landmarks(image, self.image_side)
                 )
-
-                x_min = int((self.image_side - face.size[0]) / 2)
-                x_max = int(
-                    (self.image_side - face.size[0]) / 2
-                ) + face.size[0]
-                y_min = int((self.image_side - face.size[1]) / 2)
-                y_max = int(
-                    (self.image_side - face.size[1]) / 2
-                ) + face.size[1]
-
-                curr_preprocessed = np.zeros(
-                    (3, self.image_side, self.image_side), dtype=np.uint8
-                )
-                curr_preprocessed[:, y_min:y_max, x_min:x_max] = (
-                    np.asarray(face).transpose([2, 0, 1])[::-1, ...]
-                )
-
-                preprocessed.append(curr_preprocessed)
 
             # No splits to perform under this scenario, we pack them up as if
             # they were all faces for a single image.
