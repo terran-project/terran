56ad82e12232c77f8d51962c84bc96223b3bf6f8
Agustín Azzinnari <aazzinnari@gmail.com>
2019-09-25 18:30:37 -0300

    Add `Detection` and `Recognition` to top-level `terran.face`

--- terran/face/__init__.py
+++ terran/face/__init__.py
@@ -1,2 +1,2 @@
-from terran.face.detection import face_detection  # noqa
-from terran.face.recognition import extract_features  # noqa
+from terran.face.detection import face_detection, Detection  # noqa
+from terran.face.recognition import extract_features, Recognition  # noqa
