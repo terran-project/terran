df9cf931331709b224fbfe96395ce4984a50a1fd
Agustín Azzinnari <aazzinnari@gmail.com>
2019-12-22 10:50:06 -0300

    Add function to search a directory for images

--- terran/io/__init__.py
+++ terran/io/__init__.py
@@ -1,3 +1,3 @@
 from terran.io.video.reader import open_video  # noqa
 from terran.io.video.writer import write_video  # noqa
-from terran.io.image import open_image  # noqa
+from terran.io.image import open_image, resolve_images  # noqa
