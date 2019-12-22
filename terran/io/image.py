96591322bbb1ca0ca8a64cf415199ae96a61ae24
Agustín Azzinnari <aazzinnari@gmail.com>
2019-12-22 10:44:49 -0300

    Make `open_image` support remote URLs

--- terran/io/image.py
+++ terran/io/image.py
@@ -1,30 +1,43 @@
 import numpy as np
 
+from pathlib import Path
 from PIL import Image
+from urllib.parse import urlparse
+from urllib.request import urlopen
 
 
 def open_image(uri):
     """Opens and returns image at `uri`.
+
     Always returns a HxWxC `np.array` containing the image, ready to be
     consumed by any Terran algorithm. If the image is grayscale or has an alpha
     channel, it'll get converted into `RGB`, so the number of channels will
     always be 3.
+
     Parameters
     ----------
-    uri : str
+    uri : str or pathlib.Path
         URI pointing to an image, which may be a filesystem location or a URL.
+
     Returns
     -------
     numpy.ndarray
         Array of size HxWxC containing the pixel values for the image, as
         numpy.uint8.
+
     """
-    # TODO: Allow `uri` being a list?
-    # TODO: Eventually accept a URL.
-    image = np.asarray(Image.open(uri).convert('RGB'))
+    # Check if `uri` is a URL or a filesystem location.
+    if isinstance(uri, Path):
+        image = Image.open(uri)
+    elif urlparse(uri).scheme:
+        image = Image.open(urlopen(uri))
+    else:
+        image = Image.open(Path(uri).expanduser())
+
+    image = np.asarray(image.convert('RGB'))
 
     if len(image.shape) == 2:
-        # Grayscale image.
+        # Grayscale image, turn it to a rank-3 tensor anyways.
         image = np.stack([image] * 3, axis=-1)
 
     return image
