8241e949a51e62a020a52b1cd5e1d9c4b8e054ac
Agustín Azzinnari <aazzinnari@gmail.com>
2019-12-28 10:31:24 -0300

    Mock dependencies for RTD

--- setup.py
+++ setup.py
@@ -1,4 +1,34 @@
+import os
+
 from setuptools import setup, find_packages
+
+
+INSTALL_REQUIRES = [
+    'click',
+    'ffmpeg-python',
+    'lycon',
+    'numpy',
+    'pycairo',
+    'requests',
+    'scikit-image',
+    'sklearn',
+    'torch',
+    'torchvision',
+]
+
+# Dependencies that are to be removed if the environment is `READTHEDOCS`, so
+# we don't run into troubles trying to install unsupported C-extensions. The
+# list should match `autodoc_mock_imports` in `docs/conf.py`, but with the
+# package names instead of the top-level modules.
+MOCKED_IN_DOCS = [
+    'lycon',
+    'pycairo',
+]
+
+if os.environ.get('READTHEDOCS'):
+    INSTALL_REQUIRES = list(
+        set(INSTALL_REQUIRES) - set(MOCKED_IN_DOCS)
+    )
 
 setup(
     name='terran',
@@ -9,17 +39,7 @@ setup(
     url='https://github.com/nagitsu/terran',
 
     packages=find_packages(),
-    install_requires=[
-        'click',
-        'ffmpeg-python',
-        'lycon',
-        'numpy',
-        'requests',
-        'scikit-image',
-        'sklearn',
-        'torch',
-        'torchvision',
-    ],
+    install_requires=INSTALL_REQUIRES,
 
     entry_points="""
         [console_scripts]
@@ -37,8 +57,8 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Software Development :: Libraries',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ]
 )
